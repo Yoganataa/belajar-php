dd786ac45957372b96698185567f20f0c30f7f3d
Yoganataa <arbiy494@gmail.com>
2024-04-03 00:06:11 +0700

    fix fav icon

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="./assets/img/suma.ico" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="./assets/css/suma/login.css">
     <title>SUMA | Portal</title>
